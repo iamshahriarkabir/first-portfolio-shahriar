90a13d6b7bcf06a0788c6dfaa54b7aae27c5514c
zafor <z4codelearn@gmail.com>
2025-12-08 02:41:54 +0600

    Qualification correction

--- src/components/Qualification.jsx
+++ src/components/Qualification.jsx
@@ -69,10 +69,10 @@ const Qualification = () => {
               
               {/* Exp Item 1 */}
               <div className="text-right">
-                <h3 className="font-semibold text-title">Senior Developer</h3>
-                <span className="text-sm text-text block mb-2">Tech Solutions BD</span>
+                <h3 className="font-semibold text-title">Front-End Developer</h3>
+                <span className="text-sm text-text block mb-2">Inihub</span>
                 <div className="text-xs text-text flex items-center justify-end gap-1">
-                   <Calendar size={14} /> 2023 - Present
+                   <Calendar size={14} /> Oct 2025 - Dec 2025
                 </div>
               </div>
 
@@ -92,19 +92,19 @@ const Qualification = () => {
               </div>
 
               <div>
-                <h3 className="font-semibold text-title">Frontend Dev</h3>
-                <span className="text-sm text-text block mb-2">Creative Agency</span>
+                <h3 className="font-semibold text-title">WordPress Support Engineer</h3>
+                <span className="text-sm text-text block mb-2">99BinaryLab</span>
                 <div className="text-xs text-text flex items-center gap-1">
-                   <Calendar size={14} /> 2021 - 2023
+                   <Calendar size={14} /> Jun 2025 – Oct 2025
                 </div>
               </div>
 
               {/* Exp Item 3 */}
               <div className="text-right">
-                <h3 className="font-semibold text-title">Intern</h3>
-                <span className="text-sm text-text block mb-2">Software Farm</span>
+                <h3 className="font-semibold text-title">WordPress Developer</h3>
+                <span className="text-sm text-text block mb-2">Pouch Care Digital Marketing Agency</span>
                 <div className="text-xs text-text flex items-center justify-end gap-1">
-                   <Calendar size={14} /> 2020 - 2021
+                   <Calendar size={14} /> Jan 2025 – Jun 2025
                 </div>
               </div>
 
@@ -125,19 +125,20 @@ const Qualification = () => {
               variants={fadeUp}
               className="grid grid-cols-[1fr_max-content_1fr] gap-x-6"
             >
-              
+
+
               {/* Edu Item 1 */}
               <div className="text-right">
-                <h3 className="font-semibold text-title">B.Sc in CSE</h3>
-                <span className="text-sm text-text block mb-2">Dhaka University</span>
+                <h3 className="font-semibold text-title">MERN Stack Developer</h3>
+                <span className="text-sm text-text block mb-2">Programming-Hero</span>
                 <div className="text-xs text-text flex items-center justify-end gap-1">
-                   <Calendar size={14} /> 2018 - 2022
+                   <Calendar size={14} /> 2025
                 </div>
               </div>
 
               <div className="flex flex-col items-center">
                 <span className="inline-block w-[13px] h-[13px] rounded-full bg-title"></span>
-                <span className="block w-1px h-full bg-title translate-y-[-7px]"></span>
+                <span className="block w-px h-full bg-title translate-y-[-7px]"></span>
               </div>
 
               <div></div>
@@ -151,19 +152,51 @@ const Qualification = () => {
               </div>
 
               <div>
-                <h3 className="font-semibold text-title">Web Design</h3>
-                <span className="text-sm text-text block mb-2">Udemy Course</span>
+                <h3 className="font-semibold text-title">Advanced WordPress Development</h3>
+                <span className="text-sm text-text block mb-2">SoftTech-IT Institute</span>
                 <div className="text-xs text-text flex items-center gap-1">
-                   <Calendar size={14} /> 2021
+                   <Calendar size={14} /> 2024
+                </div>
+              </div>
+              
+              {/* Edu Item 3 */}
+              <div className="text-right">
+                <h3 className="font-semibold text-title">BA (Hons)</h3>
+                <span className="text-sm text-text block mb-2">Uttara University</span>
+                <div className="text-xs text-text flex items-center justify-end gap-1">
+                   <Calendar size={14} /> 2016 - 2020
                 </div>
               </div>
 
-              {/* Edu Item 3 */}
+              <div className="flex flex-col items-center">
+                <span className="inline-block w-[13px] h-[13px] rounded-full bg-title"></span>
+                <span className="block w-px h-full bg-title translate-y-[-7px]"></span>
+              </div>
+
+              <div></div>
+
+              {/* Edu Item 4 */}
+              <div></div>
+
+              <div className="flex flex-col items-center">
+                <span className="inline-block w-[13px] h-[13px] rounded-full bg-title"></span>
+                <span className="block w-px h-full bg-title translate-y-[-7px]"></span>
+              </div>
+
+              <div>
+                <h3 className="font-semibold text-title">HSC Accounting </h3>
+                <span className="text-sm text-text block mb-2">Haluaghat Technical BM College</span>
+                <div className="text-xs text-text flex items-center gap-1">
+                   <Calendar size={14} /> 2014 - 2015
+                </div>
+              </div>
+
+              {/* Edu Item 5 */}
               <div className="text-right">
-                <h3 className="font-semibold text-title">HSC Science</h3>
-                <span className="text-sm text-text block mb-2">Dhaka College</span>
+                <h3 className="font-semibold text-title">SSC Accounting</h3>
+                <span className="text-sm text-text block mb-2">St. Andrew's High School</span>
                 <div className="text-xs text-text flex items-center justify-end gap-1">
-                   <Calendar size={14} /> 2016 - 2018
+                   <Calendar size={14} /> 2010 - 2011
                 </div>
               </div>
 
